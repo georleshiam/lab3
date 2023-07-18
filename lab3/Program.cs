1c371f8c6a1c8f9f16fa4d87ac71aa694853fa88
Georleshia Minter <georleshiam@gmail.com>
2023-07-18 12:42:25 -0500

    update

--- lab3/Program.cs
+++ lab3/Program.cs
@@ -48,97 +48,119 @@
             return product;
         }
 
-        class Program2
+
+
+        static int GetNumberFromUser(int min, int max)
         {
-            static void Main()
+            int number;
+            do
             {
-                int number = GetNumberFromUser(2, 10);
-                int[] numbers = GetNumbersFromUser(number);
+                Console.WriteLine("Please enter a number between " + min + "-" + max + ":");
+            } while (!int.TryParse(Console.ReadLine(), out number) || number < min || number > max);
 
-                double average = CalculateAverage(numbers);
+            return number;
+        }
 
-                Console.WriteLine("The average of these " + number + " numbers is: " + average);
-            }
+        static int[] GetNumbersFromUser(int count)
+        {
+            int[] numbers = new int[count];
 
-            static int GetNumberFromUser(int min, int max)
+            for (int i = 0; i < count; i++)
             {
-                int number;
+                int input;
+                bool validInput;
+
                 do
                 {
-                    Console.WriteLine("Please enter a number between " + min + "-" + max + ":");
-                } while (!int.TryParse(Console.ReadLine(), out number) || number < min || number > max);
+                    Console.WriteLine((i + 1) + " of " + count + " - Enter a number:");
+                    validInput = int.TryParse(Console.ReadLine(), out input) && input >= 0;
+                } while (!validInput);
 
-                return number;
+                numbers[i] = input;
             }
 
-            static int[] GetNumbersFromUser(int count)
-            {
-                int[] numbers = new int[count];
-
-                for (int i = 0; i < count; i++)
-                {
-                    int input;
-                    bool validInput;
-
-                    do
-                    {
-                        Console.WriteLine((i + 1) + " of " + count + " - Enter a number:");
-                        validInput = int.TryParse(Console.ReadLine(), out input) && input >= 0;
-                    } while (!validInput);
-
-                    numbers[i] = input;
-                }
-
-                return numbers;
-            }
-
-            static double CalculateAverage(int[] numbers)
-            {
-                double sum = 0;
-
-                foreach (int number in numbers)
-                {
-                    sum += number;
-                }
-
-                return sum / numbers.Length;
-            }
-            class Program3
-            {
-                public static void PrintPattern()
-                {
-                    Console.WriteLine("*");
-                    Console.WriteLine(" ");
-                    Console.WriteLine("***");
-                    Console.WriteLine(" ");
-                    Console.WriteLine("*****");
-                    Console.WriteLine(" ");
-                    Console.WriteLine("*******");
-                    Console.WriteLine(" ");
-                    Console.WriteLine("*********");
-                    Console.WriteLine(" ");
-                    Console.WriteLine("*******");
-                    Console.WriteLine(" ");
-                    Console.WriteLine("*****");
-                    Console.WriteLine(" ");
-                    Console.WriteLine("***");
-                    Console.WriteLine(" ");
-                    Console.WriteLine("*");
-                }
-                class Program4 
-                {
-public static int FindMostFrequentNumber(int[] numbers)
-                    {
-                        if (Array.Length == 0) 
-                        {
-                            throw new ArgumentException("Input array cannot be empty.");
-
-                        }
-                    }
-                }
-
-            }
-
+            return numbers;
         }
+
+        static double CalculateAverage(int[] numbers)
+        {
+            double sum = 0;
+
+            foreach (int number in numbers)
+            {
+                sum += number;
+            }
+
+            return sum / numbers.Length;
+        }
+
+        public static void PrintPattern()
+        {
+            Console.WriteLine("*");
+            Console.WriteLine(" ");
+            Console.WriteLine("***");
+            Console.WriteLine(" ");
+            Console.WriteLine("*****");
+            Console.WriteLine(" ");
+            Console.WriteLine("*******");
+            Console.WriteLine(" ");
+            Console.WriteLine("*********");
+            Console.WriteLine(" ");
+            Console.WriteLine("*******");
+            Console.WriteLine(" ");
+            Console.WriteLine("*****");
+            Console.WriteLine(" ");
+            Console.WriteLine("***");
+            Console.WriteLine(" ");
+            Console.WriteLine("*");
+        }
+
+        public static int FindMostFrequentNumber(int[] array)
+        {
+            if (array.Length == 0)
+            {
+                throw new ArgumentException("Input array cannot be empty.");
+            }
+
+            // Create a dictionary to store the frequency of each number
+            Dictionary<int, int> frequencyMap = new Dictionary<int, int>();
+
+            // Iterate through the array and update the frequency count in the dictionary
+            foreach (int num in array)
+            {
+                if (frequencyMap.ContainsKey(num))
+                {
+                    frequencyMap[num]++;
+                }
+                else
+                {
+                    frequencyMap[num] = 1;
+                }
+            }
+
+            // Variables to track the most frequent number and its frequency
+            int mostFrequentNumber = array[0];
+            int maxFrequency = frequencyMap[array[0]];
+
+            // Loop over the dictionary to find the number with the highest frequency
+            // kvp means 
+            foreach (var kvp in frequencyMap)
+            {
+                if (kvp.Value > maxFrequency)
+                {
+                    mostFrequentNumber = kvp.Key;
+                    maxFrequency = kvp.Value;
+                }
+            }
+
+            // Return the most frequent number
+            return mostFrequentNumber;
+        }
+
+
+
+
+
+
     }
 }
